1edfec2ac3922dbe3bc4d9b2dbcb2901e1fb63b4
Shu Isaka - Home <shu.h7.research@gmail.com>
2019-06-27 21:10:24 +0900

    メール設定を追加

--- src/CareerCenterSystem/CareerCenterSystem/settings.py
+++ src/CareerCenterSystem/CareerCenterSystem/settings.py
@@ -108,9 +108,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
@@ -131,3 +131,6 @@ STATICFILES_DIRS = [
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
 LOGIN_REDIRECT_URL = '/'
+
+# メールの設定ファイルを読み込む．パスワードが含まれるためgit管理下から除外．
+import CareerCenterSystem.emailsettings
